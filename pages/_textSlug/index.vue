ba9d5fe0deb3e0447c488f1774043d82e4a80756
Rafael <tecinfo.rafael@gmail.com>
2022-07-26 20:30:25 -0300

    feat: add text files and pages (closes #1)

---
+++ pages/_textSlug/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Óia eu aqui!
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <Tutorial />
+  <div>
+    <p>{{ title }}</p>
+  <div>
+      <ul>
+        <li v-for="(item, index) in items" :key="index">
+          <nuxt-link :to='{name: "textSlug", params: {"textSlug": item.slug}}'>
+            {{item.title}}
+          </nuxt-link>          
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,5 +18,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
+  layout: 'default',
+  async asyncData(context) {
+    return {
+      'title': 'Paragraph',
+      'items': await context.$content().sortBy('title').where({'published': true}).fetch(),
+    };
+  }
 })
+
 </script>
